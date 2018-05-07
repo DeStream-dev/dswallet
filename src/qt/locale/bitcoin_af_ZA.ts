22c87dc2900eccc2a017d24384d123fff0d632cd
Alexey Khvostenko <ak@its.local>
2018-05-07 20:16:57 +0300

    Miswriting fixed

--- src/qt/locale/bitcoin_af_ZA.ts
+++ src/qt/locale/bitcoin_af_ZA.ts
@@ -15,7 +15,7 @@
         <location line="+41"/>
         <source>Copyright © 2009-2014 The Bitcoin developers
 Copyright © 2012-2014 The NovaCoin developers
-Copyright © 2014-2017 The Stratis developers</source>
+Copyright © 2014-2017 The Stratis developers
 Copyright © 2018 The DeStream developers</source>
         <translation type="unfinished"/>
     </message>
